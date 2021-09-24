9ee4f1f09b76d43be1aa838b3b7be085556bf250
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-09-25 01:03:31 +0530

    template

---
+++ below1300/template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define w(n) while(n--)
+#define re return 0;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+re;
+}
