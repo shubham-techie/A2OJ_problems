6bae5ec1ced5ff8920c9215ca97dd13608bcad9f
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-09-25 18:42:10 +0530

    solved 20th problem

--- below1300/template.cpp
+++ below1300/template.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 #define f(i,a,b) for(int i=a;i<b;i++)
 #define w(n) while(n--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define re return 0;
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    fast;
 
 
 re;
