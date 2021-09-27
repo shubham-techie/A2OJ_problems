acfc79e5087c7038c00200d94c4a0001749917a9
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-09-27 18:03:28 +0530

    solved 21th problem

--- below1300/template.cpp
+++ below1300/template.cpp
@@ -2,12 +2,23 @@
 #define f(i,a,b) for(int i=a;i<b;i++)
 #define w(n) while(n--)
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define test(t) int t; cin>>t; w(t)
+#define endl "\n";
 #define re return 0;
+
 using namespace std;
+
+typedef long long int ll;
+
+void solve(){
+
+}
 
 int main(){
     fast;
 
+    test(t)
+        solve();
 
 re;
 }
