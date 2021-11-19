cb8c90594144c0ef4c2f26447cdd733242bf1d46
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-11-19 16:21:59 +0530

    solved problems

--- codedigger/B_Even_Array.cpp
+++ codedigger/B_Even_Array.cpp
@@ -6,6 +6,20 @@ void solve()
     int n;
     cin >> n;
 
+    int even(0),odd{0};
+    for(int i=0;i<n;i++){
+        int tmp; cin>>tmp;
+
+        if(i%2==0 && tmp%2!=0)
+            even++;
+        else if(i%2==1 && tmp%2==0)
+            odd++;
+    }
+
+    if(even==odd)
+        cout<<even<<"\n";
+    else 
+        cout<<-1<<"\n";
 }
 
 int main()
